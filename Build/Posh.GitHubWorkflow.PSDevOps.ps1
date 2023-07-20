1c8da427c3ce4789076a6c054ba89889acce5406
James Brundage <@github.com>
2023-07-19 23:04:12 -0700

    Initial Posh Workflow
    Fixes #2, Fixes #7, Fixes #8, Fixes #9

---
+++ Build/Posh.GitHubWorkflow.PSDevOps.ps1
@@ -0,0 +1,10 @@
+﻿#requires -Module Posh
+#requires -Module PSDevOps
+Import-BuildStep -ModuleName Posh
+Push-Location ($PSScriptRoot | Split-Path)
+New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -On Demand, Push -Job TagReleaseAndPublish,
+    BuildPosh -OutputPath (
+        Join-Path $pwd .\.github\workflows\TestReleaseAndPublish.yml
+    )    
+
+Pop-Location
