3435d100a2127076fc73eb252a22c123f67f5a18
James Brundage <@github.com>
2023-07-20 00:18:29 -0700

    Adding Process Heatmap (Fixes #52)

---
+++ Formatting/System.Diagnostics.Process.format.ps1
@@ -0,0 +1,33 @@
+﻿Write-FormatControl -Name ProcessGroupControl -Action {
+    Write-FormatViewExpression -ScriptBlock {
+        . $Indent $_.ProcessName
+    }
+    Write-FormatViewExpression -Newline
+    Write-FormatViewExpression -ScriptBlock {
+        . $Indent $_.Path
+    }
+}
+
+Write-FormatView -TypeName System.Diagnostics.Process -Property Handles, 'NPM(K)', 'PM(K)','WS(K)','CPU(S)', 'ID', 'SI','ProcessName' -VirtualProperty @{
+    'NPM(K)' = { [long]($_.NPM / 1024) } 
+    'PM(K)' = { [long]($_.PM / 1024) }
+    'WS(K)' = { [long]($_.WS / 1024) }
+    'CPU(S)' = { 
+        if ($_.CPU -ne $()) { 
+            $_.CPU.ToString("N") 
+        }
+    }    
+} -ColorProperty @{
+    'NPM(K)' = {
+        Format-Heatmap -InputObject $_.NPM -HeatMapMin 16mb -HeatMapMiddle .5gb -HeatMapMax 1gb 
+    }
+    'PM(K)' = { 
+        Format-Heatmap -InputObject $_.PM -HeatMapMin 16mb -HeatMapMiddle .5GB -HeatMapMax 1gb 
+    }
+    'WS(K)' = {
+        Format-Heatmap -InputObject $_.WS -HeatMapMin 16mb -HeatMapMiddle 512mb -HeatMapMax 1gb 
+    }  
+} -Name Process.Heatmap
+
+
+Write-FormatView -TypeName System.Diagnostics.Process -Property ID -AsList -GroupByProperty ProcessName -GroupAction ProcessGroupControl -Name process
