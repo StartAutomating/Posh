7e038356956e4b975f73614c7e0756bcb3dbe025
James Brundage <@github.com>
2023-08-11 19:41:31 -0700

    Updating Posh.Preset.Command.Use help (re #154)

--- Types/Posh.Preset.Command/Use.ps1
+++ Types/Posh.Preset.Command/Use.ps1
@@ -1,8 +1,10 @@
 <#
 .SYNOPSIS
-    
+    Uses a preset
 .DESCRIPTION
+    Uses or applies a Posh preset.
 
+    Posh presets will take the data in a *.preset.* file and apply it to a variable.    
 #>
 
 $importedData = 
