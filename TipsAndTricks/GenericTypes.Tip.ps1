dc0a800e781e059279bc06426fa19b9ebe157ae8
James Brundage <@github.com>
2023-08-11 20:04:24 -0700

    Adding GenericTypes.Tip.ps1 (Fixes #167)

--- TipsAndTricks/GenericTypes.Tip.ps1
+++ TipsAndTricks/GenericTypes.Tip.ps1
@@ -1,7 +1,9 @@
 # You can use generic types in PowerShell.
 
 # Just include inner types in inner brackets
-[Collections.Generic.List[BigInt]]::new([BigInt[]]@(0,1))
+[Collections.Generic.List[BigInt]]::new(
+    [BigInt[]]@(0,1)
+)
 
 # Whitespace does not matter too much.
 [Collections.Generic.Dictionary[
