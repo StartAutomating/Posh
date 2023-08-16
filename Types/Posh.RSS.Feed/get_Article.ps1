a652f8944e86ee63a7c1c982129377342ebd697f
James Brundage <@github.com>
2023-08-15 21:28:20 -0700

    Adding POSH.RSS.Feed Article (Fixes #178)

---
+++ Types/Posh.RSS.Feed/get_Article.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    Gets articles in a feed.
+.DESCRIPTION
+    Gets the articles in a RSS feed.
+
+    Articles will be cached for performance.
+.EXAMPLE
+    $Posh.News.Article # Get all articles from all feeds associated with Posh.
+#>
+if (-not $this.'.Article') {    
+    Add-Member -InputObject $this -MemberType NoteProperty -Name '.Article' -Value @(
+        foreach ($feedItem in Invoke-RestMethod -Uri $this.Url) {
+            $feedItem.pstypenames.insert(0,'Posh.RSS.Article')
+            $feedItem | 
+                Add-Member NoteProperty Source $this.Name -Force -PassThru
+        }        
+    ) -Force
+}
+$this.'.Article'
