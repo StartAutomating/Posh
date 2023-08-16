cdcf00cb93d3c6dac86fcf066b93bd15c3705a90
James Brundage <@github.com>
2023-08-16 13:27:16 -0700

    Posh.RSS.FeedCollection - get_RecentDuration
    (adding help) (re #191)

--- Types/Posh.RSS.FeedCollection/get_RecentDuration.ps1
+++ Types/Posh.RSS.FeedCollection/get_RecentDuration.ps1
@@ -1,3 +1,11 @@
+<#
+.SYNOPSIS
+    Gets the duration considered recent
+.DESCRIPTION
+    Gets the duration that is used to determine how recent an article is.
+
+    By default, this is 45 days.    
+#>
 if (-not $this.'.RecentDuration') {
     $this | 
         Add-Member NoteProperty '.RecentDuration' (New-TimeSpan -Days 45) -Force
