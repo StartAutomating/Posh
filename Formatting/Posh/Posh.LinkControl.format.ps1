2c50f7998e41ad123b29070159e71cec274c3fe1
James Brundage <@github.com>
2023-08-16 13:42:37 -0700

    Adding Posh.Link.Line control (Fixes #196)

--- Formatting/Posh/Posh.LinkControl.format.ps1
+++ Formatting/Posh/Posh.LinkControl.format.ps1
@@ -16,3 +16,23 @@ Write-FormatView -TypeName n/a -Name "Posh.Link" -AsControl -Action {
     }    
     ) -join ''
 }
+
+Write-FormatView -TypeName n/a -Name "Posh.Link.Line" -AsControl -Action {
+    @(
+    if ($_.Name -and $_.Url) {
+        if ($psStyle.FormatHyperlink -and -not $env:GITHUB_WORKSPACE) {
+            $psStyle.FormatHyperlink($_.Name, $_.Url)
+        } else {
+            "[$($_.Name)]($($_.Url))"
+        }
+    } elseif ($_.Url) {
+        $uri = [uri]$_.Url
+        if ($psStyle.FormatHyperlink -and -not $env:GITHUB_WORKSPACE) {
+            $psStyle.FormatHyperlink($_.Url, $uri)
+        } else {
+            "[$($_.Url)]($($uri))"
+        }        
+    }
+    [Environment]::NewLine
+    ) -join ''
+}
