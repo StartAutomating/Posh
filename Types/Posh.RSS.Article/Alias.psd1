dca952d35cefab3d2a684ce18cd4d1dd6d21ee0b
James Brundage <@github.com>
2023-08-15 21:34:06 -0700

    Adding Posh.RSS.Article.get_DatePublished Help (Fixes #183)
    Also, aliasing properties (Fixes #185)

---
+++ Types/Posh.RSS.Article/Alias.psd1
@@ -0,0 +1,4 @@
+@{
+    Name = "Title"
+    Url  = "Link"
+}
--- Types/Posh.RSS.Article/get_DatePublished.ps1
+++ Types/Posh.RSS.Article/get_DatePublished.ps1
@@ -1,4 +1,13 @@
+<#
+.SYNOPSIS
+    Gets an article's date published
+.DESCRIPTION
+    Gets the date an article was published, as a `[DateTime]`.
+.EXAMPLE
+    $Posh.News.Article.DatePublished
+#>
 $pubDateTime = $this.pubDate -as [DateTime]
+
 if ($pubDateTime) {
     $pubDateTime
 }
