08f63fa9e52bc5b4b9ab792d774ac70b803251c4
James Brundage <@github.com>
2023-08-26 13:46:16 -0700

    Adding VersionControl (Fixes #230)

---
+++ Formatting/VersionControl.format.ps1
@@ -0,0 +1,21 @@
+Write-FormatView -TypeName n/a -Name VersionControl -Action {
+    Write-FormatViewExpression -ScriptBlock {
+        if ($PSStyle) {
+            @(foreach ($versionPart in $_.ToString() -split "\.") {
+                @(
+                    $PSStyle.Foreground.Cyan
+                    $PSStyle.Bold
+                    $versionPart
+                    $PSStyle.Reset
+                ) -join ''            
+            }) -join "$(
+                @(
+                    $PSStyle.Foreground.Yellow
+                    '.'                
+                ) -join ''      
+            )"
+        } else {
+            $_.ToString()
+        }
+    }
+} -AsControl
