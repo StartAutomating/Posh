af09155176fbeba7a58e6f4e8f8f4a1a802e3c7e
James Brundage <@github.com>
2023-08-26 14:44:22 -0700

    Adding TypeInterfaceControl (Fixes #220)

---
+++ Formatting/Reflection/TypeInterfaceControl.format.ps1
@@ -0,0 +1,5 @@
+﻿Write-FormatView -Action {
+    Write-FormatViewExpression -Newline
+    Write-FormatViewExpression -ScriptBlock { '  *' }
+    Write-FormatViewExpression -ControlName TypeNameControl -ScriptBlock  {$_ } -Style 'Foreground.Cyan', 'Bold'
+} -TypeName n/a -Name TypeInterfaceControl -AsControl
