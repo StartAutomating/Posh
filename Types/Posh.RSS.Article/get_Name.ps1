2ecde355c9d8c6408ffafd5ac0e31a3b896c183f
James Brundage <@github.com>
2023-08-26 17:31:38 -0700

    Updating Posh.RSS.Article.Name (Fixes #229)

--- Types/Posh.RSS.Article/get_Name.ps1
+++ Types/Posh.RSS.Article/get_Name.ps1
@@ -4,8 +4,15 @@
 .DESCRIPTION
     Gets the name of an article in an RSS feed.
 #>
-if ($this.Title -isnot [string]) {
-    $this.Title.InnerText
-} else {
+if ($this.Title -and $this.Title -isnot [string]) {
+    if ($this.Title.InnerText) {
+        $this.Title.InnerText
+    } elseif ($this.Title -is [object[]]) {
+        $this.Title | Select-Object -Unique -First 1
+    }    
+} elseif ($this.Title) {
     $this.Title
+} elseif ($this.URL) {
+    $thisURI = $this.URL -as [uri]
+    $thisURI.Segments[-1] -replace '-', ' '
 }
