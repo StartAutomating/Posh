70cbb54944c24ca0e5e115cb7c1870dac7269f45
James Brundage <@github.com>
2024-02-25 10:56:17 -0800

    feat: Posh.Host.UI.Box.BottomMiddle ( Fixes #362 )
    Fixing for single lines

--- Types/Posh.Host.UI.Box/get_BottomMiddle.ps1
+++ Types/Posh.Host.UI.Box/get_BottomMiddle.ps1
@@ -3,12 +3,19 @@
     Gets a box's Bottom Middle character
 .DESCRIPTION
     Gets the Bottom Middle character in a box
+.NOTES
+    If there is only one line, and it is shorter than 5 characters, then the Bottom Left will be considered the Bottom Middle
 #>
+
 $theseLines = @($this -split '[\r\n]+' -ne '')
 
 if (-not $theseLines) { return }
-    $theseLines[-1][
-        [Math]::Floor($theseLines[-1].Length / 2)
-    ]    
-
-
+if ($theseLines.Length -eq 1 -and $theseLines[0].Length -lt 5) {
+    # If there is only one line, and it is less than five characters
+    # Use the upper left as the middle character.
+    $theseLines[0].Substring(0,1)
+} else {
+    $theseLines[-1].Substring(
+        ([Math]::Floor($theseLines[0].Length / 2)), 1
+    )    
+}
